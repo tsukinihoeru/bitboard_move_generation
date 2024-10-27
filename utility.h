5f1b1b948edfbb66b19f7b1461996b339317e4d0
tsukinihoeru <104182063+tsukinihoeru@users.noreply.github.com>
2024-10-27 17:52:58 -0500

    Added Zobrist Hashing

---
+++ utility.h
@@ -0,0 +1,54 @@
+//
+//  utility.h
+//  Invincible_Summer
+//
+//  Created by Harry Chiu on 10/27/24.
+//
+#include <cassert>
+
+// xorshift64star Pseudo-Random Number Generator
+// This class is based on original code written and dedicated
+// to the public domain by Sebastiano Vigna (2014).
+// It has the following characteristics:
+//
+//  -  Outputs 64-bit numbers
+//  -  Passes Dieharder and SmallCrush test batteries
+//  -  Does not require warm-up, no zeroland to escape
+//  -  Internal state is a single 64-bit integer
+//  -  Period is 2^64 - 1
+//  -  Speed: 1.60 ns/call (Core i7 @3.40GHz)
+//
+// For further analysis see
+//   <http://vigna.di.unimi.it/ftp/papers/xorshift.pdf>
+
+//using seed PRNG rng(1070372);
+
+class PRNG {
+
+    uint64_t s;
+
+
+   public:
+    PRNG(uint64_t seed) :
+        s(seed) {
+        assert(seed);
+    }
+
+    template<typename T>
+    T rand() {
+        return T(rand64());
+    }
+
+    uint64_t rand64() {
+
+        s ^= s >> 12, s ^= s << 25, s ^= s >> 27;
+        return s * 2685821657736338717LL;
+    }
+    
+    // Special generator used to fast init magic numbers.
+    // Output values only have 1/8th of their bits set on average.
+    template<typename T>
+    T sparse_rand() {
+        return T(rand64() & rand64() & rand64());
+    }
+};
